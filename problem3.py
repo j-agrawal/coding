619f459f5174f12470bf7e600cc302625e4afd2b
jayeshag <jayeshagrawal896@gmail.com>
2019-06-11 18:20:40 +0000

    Add files via upload

---
+++ problem3.py
@@ -0,0 +1,13 @@
+adhoc=[1,2,3,1,4,5,66,22,2,6,0,9]
+k = []
+j = []
+for i in adhoc:
+    if(i>5):
+        k.append(i)
+        
+    if(i<=2):
+         j.append(i)
+         
+    
+print(k)
+print(j)
